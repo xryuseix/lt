91d3cf00e565246608cfde2d6b6b9181e3c2333a
xryuseix <ryusei143.shootingstar@gmail.com>
2021-08-15 20:35:05 +0900

    fix: github pagesのファイルパスがわからん

--- deploy.py
+++ deploy.py
@@ -132,8 +132,9 @@ def main():
                 for image in [
                     image for image in ls(pwd()) if endswith_array(image, image_ext)
                 ]
-            ],('href="/lt/lt/"', 'href="/lt/"')
+            ]
         )
+        patterns.extend(('href="/lt/lt/"', 'href="/lt/"'))
         cd("assets")
         vendor_file_name = [
             file
